f495c154c0571982290bf08c579821b721ba2fb8
Ramsfield <mgriffith004@gmail.com>
2018-03-01 11:55:43 -0800

    Added insult to injury

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# Chico Bot 
 # ChicoBot
+Welcome to the Chico Bot! If you have any questions, comments, or concerns, please contact me: Ramsfield#7696
--- app.js
+++ app.js
@@ -17,6 +17,7 @@ client.on('message', message => {
 
 	console.log(message.author.username + ' said: ' + message.content);
 
+	//Helper function
 	if(message.content === prefix + "help")
 	{
 		message.channel.send({embed: {
@@ -32,79 +33,98 @@ client.on('message', message => {
 				name:"Moderation:",
 				value: "**~mute `@user`** -- ***__Requires Lvl 15 - Monkey Face__*** -- Mutes a user\n**~unmute `@user`** -- ***__Requires Lvl 15 - Monkey Face__*** -- Unmutes a user"
 			}],
-			timestamp: new Date(),
-			footer: {
-				icon_url: client.user.avatarURL,
-				text: message.author.username
-			}
-			}
+				timestamp: new Date(),
+				footer: {
+					icon_url: client.user.avatarURL,
+					text: message.author.username
+				}
+		}
 		});}
 		//Mute function
 		//Checks if member is of role "Lvl 15 - Monkey Face"
-		if(message.content.startsWith(prefix+'mute')){
+	else if(message.content.startsWith(prefix+'mute')){
 
-			if(!message.member.roles.some(r=>["Lvl 15 - Monkey Face"].includes(r.name))){
+		if(!message.member.roles.some(r=>["Lvl 15 - Monkey Face"].includes(r.name))){
+			return message.channel.send({embed: {
+				color: embed_color,
+				description: message.author.username + " is not in the sudoers file. This incident will be reported."
+			}}); 
+		}
+		else
+		{
+			let member = message.mentions.members.first();
+			if(!member) //Check if member is in guild
 				return message.channel.send({embed: {
 					color: embed_color,
-					description: message.author.username + " is not in the sudoers file. This incident will be reported."
-				}}); 
-			}
-			else
-			{
-				let member = message.mentions.members.first();
-				if(!member) //Check if member is in guild
-					return message.channel.send({embed: {
-						color: embed_color,
-						description: "Please enter a valid member of this server."
-					}});
-				else if(member === message.member) //Check if member is author
-					return message.channel.send({embed: {
-						color: embed_color,
-						description: message.author.username + " don't do this to yourself!"
-					}});
-				else if(member.roles.some(r=>["Mods"].includes(r.name))) //Check if member is a Mod
-					return message.channel.send({embed: {
-						color: embed_color,
-						description: "I'm sorry Dave, I'm afraid I can't do that."
-					}});
-				else if(member.id === '412109529711378472') //Check if trying to mute bot
-					return message.channel.send({embed: {
-						color: embed_color,
-						description: "Don't bring me down, Bruce."
-					}});
-				else{
-					let muted = message.guild.roles.find("name", "Muted");
-					member.addRole(muted).catch(console.error);
-					return message.channel.send({embed: {
-						color: embed_color,
-						description: member + " has been muted. Mods will be notified."
-					}});
-				}
-			}
-		}
-		//Unmute function. Basically a copy paste of mute
-		if(message.content.startsWith(prefix+'unmute')){
-
-			if(!message.member.roles.some(r=>["Lvl 15 - Monkey Face"].includes(r.name))){
-				return message.reply(" is not in the sudoers file. This incident will be reported."); 
-			}
-			else
-			{
-				let member = message.mentions.members.first();
-				if(!member)
-					return message.reply(" Please enter a valid member of this server.");
+					description: "Please enter a valid member of this server."
+				}});
+			else if(member === message.member) //Check if member is author
+				return message.channel.send({embed: {
+					color: embed_color,
+					description: message.author.username + " don't do this to yourself!"
+				}});
+			else if(member.roles.some(r=>["Mods"].includes(r.name))) //Check if member is a Mod
+				return message.channel.send({embed: {
+					color: embed_color,
+					description: "I'm sorry Dave, I'm afraid I can't do that."
+				}});
+			else if(member.id === '412109529711378472') //Check if trying to mute bot
+				return message.channel.send({embed: {
+					color: embed_color,
+					description: "Don't bring me down, Bruce."
+				}});
+			else{
 				let muted = message.guild.roles.find("name", "Muted");
-				member.removeRole(muted).catch(console.error);
-				return message.reply(member + " has been unmuted.");
+				member.addRole(muted).catch(console.error);
+				return message.channel.send({embed: {
+					color: embed_color,
+					description: member + " has been muted. Mods will be notified."
+				}});
 			}
 		}
+	}
+	//Unmute function. Basically a copy paste of mute
+	else if(message.content.startsWith(prefix+'unmute')){
 
-		if(message.content === prefix + 'ping'){
-			message.channel.send({embed: {
+		if(!message.member.roles.some(r=>["Lvl 15 - Monkey Face"].includes(r.name))){
+			return message.channel.send({embed: {
 				color: embed_color,
-				description:` \`${Date.now() - message.createdTimestamp} ms\` `
-			}});
+				description:"You have no power here."
+			}}); 
 		}
-	});
+		else
+		{
+			let member = message.mentions.members.first();
+			if(!member)
+				return message.reply(" Please enter a valid member of this server.");
+			let muted = message.guild.roles.find("name", "Muted");
+			member.removeRole(muted).catch(console.error);
+			return message.reply(member + " has been unmuted.");
+		}
+	}
 
-	client.login(token);
+
+	else if(message.content === prefix + 'ping'){
+		message.channel.send({embed: {
+			color: embed_color,
+			description:` \`${Date.now() - message.createdTimestamp} ms\` `
+		}});
+	}
+
+	else if(message.content.startsWith(prefix+'insult'))
+	{
+		var x = Math.floor(Math.random()*3);
+		let insulted = message.mentions.members.first();
+		if(!insulted)
+			insulted = message.author;
+		if(x === 0)
+			insulted = insulted+(", your mother was a hamster and your father smelt of elderberries.");
+		else if (x === 1)
+			insulted = insulted+(", I bet you put your shoes on before your socks.");
+		else if (x === 2)
+			insulted = insulted+(", I can't. It's too easy.");
+		message.channel.send(insulted);
+	}
+});
+
+client.login(token);
